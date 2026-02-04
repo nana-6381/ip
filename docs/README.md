70ea339d5e5e27491f1883d2a45a04467604cec5
nana-6381 <190936945+nana-6381@users.noreply.github.com>
2026-02-04 14:44:51 +0800

    Level-0: Rename, greet, exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Kiki User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Kiki.java
@@ -0,0 +1,14 @@
+public class Kiki {
+    public static void main(String[] args) {
+        String nameAlt = "(づ｡◕‿‿◕｡)づ  K i k i\n";
+        String logo = " _  __ _ _    _\n"
+                + "| |/ /(_) | _(_)\n"
+                + "| ' / | | |/ / |\n"
+                + "| . \\ | |   <| |\n"
+                + "|_|\\_\\|_|_|\\_\\_|\n";
+        System.out.println("Hey what's up, from\n" + logo);
+        System.out.println("How can I help you today?");
+        System.out.println("Bye, hope to see you soon.");
+        System.out.println("- " + nameAlt);
+    }
+}
