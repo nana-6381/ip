a2d920bddef42461bdbe4bfb8110ed72d78167d1
nana-6381 <190936945+nana-6381@users.noreply.github.com>
2026-02-06 15:33:21 +0800

    A-TextUiTesting: Add automated text UI tests

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -9,7 +9,7 @@ fi
 # delete output from previous run
 if [ -e "./ACTUAL.TXT" ]
 then
-    rm ACTUAL.TXT
+    rm -f ACTUAL.TXT EXPECTED-UNIX.TXT
 fi
 
 # compile the code into the bin folder, terminates if error occurred
@@ -20,11 +20,13 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin Kiki < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+# dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+# removed for macOS-friendly command to run
+sed -i '' -e 's/\r$//' ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
